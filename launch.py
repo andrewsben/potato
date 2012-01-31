b30d569f2e5e3fe20cb378cb7e721a2b4603cc44
Ben Andrews <andrewsben@gmail.com>
2012-01-31 16:19:09 -0500

    added functionality to setup / check security group rules for 22.  add floating ip to server.  then paramiko to floating ip and do ping test, and print results

--- launch.py
+++ launch.py
@@ -1,14 +1,148 @@
 #!/usr/bin/env python
 
+import os
+import paramiko
 import random
 import sys
 import time
 from novaclient.v1_1 import client
 
+rate_limit_sleep_time = 10
+floating_ips_max_check = 10
+nc = False
+
+
+def check_sec_group():
+    try:
+        sec_group_name = "default"
+        sec_group_object = False
+        has_port = False
+
+        for sec_group in nc.security_groups.list():
+            if sec_group.name == sec_group_name:
+                sec_group_object = sec_group
+
+        for rule in sec_group.rules:
+            if rule['from_port'] == 22:
+                has_port = True
+
+        if not has_port:
+            nc.security_group_rules.create(sec_group_object.id,
+                                                'tcp', 22, 22, '0.0.0.0/0')
+
+    except Exception as ex:
+        print ex
+        return False, ex
+    return True, sec_group_object
+
+
+def get_key():
+    """create ssh keys to connect to server"""
+    key_name = "key-%d-%d" % (time.time(), random.randint(0, 99999999))
+    dummy = os.popen('ssh-keygen -t rsa -P "" -f %s' % key_name).readlines()
+    public_key = open('%s.pub' % key_name, 'r').readlines()[0]
+    created_key = nc.keypairs.create(key_name.split('/')[-1], public_key)
+    return created_key, key_name
+
+
+def assign_floating_ip(server_name):
+
+    server = False
+    for s in nc.servers.list():
+        if s.name == server_name:
+            server = s
+
+    if server:
+        ip = get_floating_ip()
+        if ip:
+            server.add_floating_ip(ip)
+            return ip
+
+    return False
+
+
+def get_floating_ip():
+
+    dont_use = ['50.56.12.240', '50.56.12.241', '50.56.12.242', '50.56.12.243']
+
+    for floating_ip in nc.floating_ips.list():
+        if floating_ip.instance_id == None and floating_ip.ip not in dont_use:
+            return floating_ip.ip
+
+    try:
+        for x in range(floating_ips_max_check):
+            floating_ip = nc.floating_ips.create()
+            if floating_ip.ip not in dont_use:
+                return floating_ip.ip
+    except:
+        return False
+
+    return False
+
+
+def check_rate_limited(message):
+
+    redo = False
+    msg_check = ['This request was rate-limited. (HTTP 413)',
+                'This request was rate-limited. (HTTP 403)']
+
+    if str(message) in msg_check:
+        time.sleep(rate_limit_sleep_time)
+        redo = True
+    return redo
+
+
+def connect_to_server(ssh_connect_info, port=22):
+    """return ssh connection"""
+
+    server_ip = ssh_connect_info['ip']
+    login_name = ssh_connect_info['login']
+    key_location = ssh_connect_info['key']
+    connection_good = False
+
+    x = 0
+    while not connection_good:
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(server_ip, username=login_name,
+                                key_filename=key_location)
+            return ssh, "SSH connection active"
+        except Exception as ex:
+            if x < 10:
+                time.sleep(2)
+            else:
+                return False, "Could not complete ssh connection"
+
+    return False, "Shouldn't get here"
+
+
+def ping_thing(ssh_connection_dict):
+
+    check_sec_group()
+    pingers = ['4.4.4.4', 'www.google.com']
+    for pingee in pingers:
+        ssh, return_str = connect_to_server(ssh_connection_dict)
+        if ssh:
+            stdin, stdout, stderr = ssh.exec_command('ping -w 3 %s' % pingee)
+            channel = stdout.channel
+            status = channel.recv_exit_status()
+            ping_return = stdout.readlines()
+            for line in ping_return:
+                if "transmitted" in line:
+                    transmitted = line.split(',')[0].lstrip().split(' ')[0]
+                    received = line.split(',')[1].lstrip().split(' ')[0]
+                    packet_loss = line.split(',')[2].lstrip().split(' ')[0]
+                    print "Ping Results %s: %s/%s %s loss" % (pingee,
+                                received, transmitted, packet_loss)
+            ssh.close()
+            channel.close()
+
 
 def launch(auth_url, tenant, user, password, destroy_time=60, boot_time=60):
     """launch and terminate a VM within a specified time"""
 
+    global nc
     nc = client.Client(user, password, tenant, auth_url)
     name = "test-%d-%d" % (time.time(), random.randint(0, 99999999))
 
@@ -29,9 +163,13 @@ def launch(auth_url, tenant, user, password, destroy_time=60, boot_time=60):
     assert flavor, "No satisfying flavor"
     print flavor
 
+    nova_key, local_key = get_key()
+    assert nova_key, "Error with key"
+
     new_server = nc.servers.create(image=image,
                                    flavor=flavor,
-                                   name=name)
+                                   name=name,
+                                   key_name=nova_key.name)
     print new_server
 
     server_id = new_server.id
@@ -50,6 +188,13 @@ def launch(auth_url, tenant, user, password, destroy_time=60, boot_time=60):
         else:
             time.sleep(3)
 
+    #create or assign floating ip
+    floating_ip = assign_floating_ip(name)
+    if floating_ip:
+        ssh_connect_info = {'ip': floating_ip, 'login': 'ubuntu',
+                                        'key': local_key}
+        ping_thing(ssh_connect_info)
+
     nc.servers.delete(server_id)
     is_del = False
     start = time.time()
@@ -60,10 +205,15 @@ def launch(auth_url, tenant, user, password, destroy_time=60, boot_time=60):
         else:
             time.sleep(1)
 
+    #clean up.  remove keys local and for nova
+    os.system('rm %s*' % local_key)
+    nova_key.delete()
+
     if not booted and not is_del:
         print "Server %s not booted within %d sec" % (name, boot_time)
     assert is_del, "Server %s not deleted within %d sec" % (name, destroy_time)
     assert booted, "Server %s not booted within %d sec" % (name, boot_time)
+    assert floating_ip, "Could not get floating ip"
 
 if __name__ == '__main__':
     try:
